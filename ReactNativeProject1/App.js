c904fbc2c865ac2a028ecd7169c507138dcd8b06
nicywi@gmail.com <nicywi@gmail.com>
2024-04-17 14:43:46 +0200

    layout added

--- ReactNativeProject1/App.js
+++ ReactNativeProject1/App.js
@@ -1,52 +1,23 @@
-import { StyleSheet, SafeAreaView, Text, View, Button, TouchableOpacity } from 'react-native';
+
+import { StyleSheet, TextInput, Button, Text, View} from 'react-native';
 
 const Separator = () => <View style={styles.separator} />;
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-    <View>
-      <Text style={{margin: 20, borderColor: 'pink', 
-      borderWidth: 6, padding: 20, backgroundColor: 'yellow'}}>
-        Hello from new App</Text>
-      <Button
-        title="Press me"
-        onPress={() => Alert.alert('Cannot press this one')}
-      />
-      <Separator />
-      <Button title="Tap here!" style={styles.button} />
-      <TouchableOpacity onPress={() => console.log('Tapped!')} style={styles.button}>
-          <Text style={styles.buttonText}>Tap here!</Text>
-        </TouchableOpacity>
-    
+    <View style={styles.appContainer}>
+      <View>
+        <TextInput placeholder='Your course goal!'></TextInput>
+        <Button title="Add Goal"/>
+      </View>
+      <View>
+        <Text>List og goals...</Text>
+      </View>
     </View>
-     <Separator />
-     </SafeAreaView>
   );
 }
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  button: {
-    backgroundColor: 'greenyellow',
-    justifyContent: 'center',
-    alignItems: 'center',
-    margin: 20,
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 5,
-  },
-  buttonText: {
-    color: 'mediumslateblue', 
-    fontSize: 16,
-  },
-  separator: {
-    marginVertical: 8,
-    borderBottomColor: 'red',
-    borderBottomWidth: 2,
-  },
+  appContainer: {
+    padding: 50
+  }
 });
