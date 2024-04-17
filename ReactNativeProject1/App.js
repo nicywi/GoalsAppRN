e8dbd37f3e69538509b32ae03b172221ababc1e0
nicywi@gmail.com <nicywi@gmail.com>
2024-04-17 19:33:13 +0200

    added styling to main page

--- ReactNativeProject1/App.js
+++ ReactNativeProject1/App.js
@@ -4,13 +4,19 @@ import { StyleSheet, TextInput, Button, Text, View} from 'react-native';
 const Separator = () => <View style={styles.separator} />;
 
 export default function App() {
+
+  function goalInputHandler(){};
+  function addGoalHandler(){};
+
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder='Your course goal!'></TextInput>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} 
+        placeholder='Your course goal!' 
+        onChangeText={goalInputHandler}/>
         <Button title="Add Goal"/>
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List og goals...</Text>
       </View>
     </View>
@@ -18,6 +24,27 @@ export default function App() {
 }
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50
-  }
+    paddingTop: 50,
+    paddingHorizontal: 16,
+    flex: 1
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderColor: 'green'
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 8,
+    padding: 8
+  },
+  goalsContainer: {
+  flex: 5
+  },
 });
